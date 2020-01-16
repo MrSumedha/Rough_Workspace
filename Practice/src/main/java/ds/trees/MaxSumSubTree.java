aa6d03c7de3ac013e1610ee01ceb1b8cc4935b23
MrSumedha <mrsumedha777@gmail.com>
2020-01-16 10:12:39 +0530

    MaxSum subtree

---
+++ Practice/src/main/java/ds/trees/MaxSumSubTree.java
@@ -0,0 +1,17 @@
+package ds.trees;
+
+public class MaxSumSubTree {
+	class Node {
+		int val;
+		Node right, left;
+		public Node(int val) {
+			this.val = val;
+			this.left = this.right = null;
+		}
+	}
+	
+	public int maxSum(Node root) {
+		
+		return 0;
+	}
+}
